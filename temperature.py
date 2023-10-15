bdb0e4b515b58042750e93fad9f80edea54a8602
NamelessTaken <147025484+NamelessTaken@users.noreply.github.com>
2023-10-15 10:35:23 +0530

    temperature.py
    Phone

---
+++ temperature.py
@@ -0,0 +1,9 @@
+Temperature=(int(input("Temperature:")))
+if Temperature > 35:
+   print("it's higher than room temperature ")
+   print("drink water")
+elif Temperature <30:
+   print("temperature is below room temperature")
+   print("no need of a.c")
+else:
+   print("moderate temperature")
