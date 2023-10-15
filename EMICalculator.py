89c2517d27feeeb0788fa8b6c3caa8a65468e0ff
NamelessTaken <147025484+NamelessTaken@users.noreply.github.com>
2023-10-15 15:43:57 +0530

    Update EMICalculator.py

---
+++ EMICalculator.py
@@ -0,0 +1,6 @@
+p = float(input("Enter principal amount: "))
+R = float(input("Enter annual interest rate: "))
+n = int(input("Enter number of months: " ))
+r= R/(12*100)
+emi = p * r * ((1+r)**n)/((1+r)**n - 1)
+print("Monthly EMI = ", emi)
